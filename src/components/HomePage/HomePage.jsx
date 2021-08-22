ae4bce2e1532bf3b3b0f9477f6ff3ab7b1a122a8
Kyle Davelaar <kyledavelaar@gmail.com>
2021-08-22 16:35:25 -0700

    add smooth scroll behavior

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -5,25 +5,34 @@ import JerryThumb from '../../images/jerry-thumb.png';
 
 
 const HomePage = () => (
-  <div>
+  <div id={styles.wrapper}>
     <Navbar />
-    <div className={styles.banner}>
-      <h1 className={styles.header}>Jerry Davelaar</h1>
-      <h4 className={styles.header2}>MFT, MA</h4>
-    </div>
-    <div className={styles.mainContainer}>
-      <div className={styles.left}>
-        <img className={styles.jerryThumb} src={JerryThumb} alt="Jerry Davelaar" />
+    <section className={styles.content}>
+      <div className={styles.banner}>
+        <h1 className={styles.header}>Jerry Davelaar</h1>
+        <h4 className={styles.header2}>MA, MDIV</h4>
       </div>
-      <div className={styles.right}>
-        <div className={styles.bodyContainer}>
-          <p className={styles.body}>After working as a therapist for a few years, Jerry served for five years as an ordained associate pastor and then ten years as a senior pastor. Since then, Jerry has returned to his work as a therapist, pursuing his passion to understand people and bring hope to their desire for change and growth.</p>
-          <p className={styles.body}>Now that Jerry has returned to his work as a therapist, he enjoys working with individuals, couples and families. Some of the struggles that his clients come with are: anxiety, depression, anger, fear, loneness, lack of confidence, guilt, shame, minority issues. Jerry also works with men wanting help with issues like substance abuse, pornography, and dysfunctional familial dynamics. Often he finds that his patient’s lives have been effected by past and/or current traumas. Jerry’s special interest is working with pastors and church staff regarding personal and ministry issues.</p>
-          <p className={styles.body}>Jerry has been married 40 plus years with three married children and five grandchildren. After growing up on a farm, he and his wife began their life together in the farming business. Throughout all the career changes, their faith in God has always been what they have leaned on for strength.</p>
+      <div className={styles.mainContainer}>
+        <div className={styles.left}>
+          <img className={styles.jerryThumb} src={JerryThumb} alt="Jerry Davelaar" />
         </div>
+        <div className={styles.right}>
+          <div className={styles.bodyContainer}>
+            <p className={styles.body}>After working as a therapist for a few years, Jerry served for five years as an ordained associate pastor and then ten years as a senior pastor. Since then, Jerry has returned to his work as a therapist, pursuing his passion to understand people and bring hope to their desire for change and growth.</p>
+            <p className={styles.body}>Now that Jerry has returned to his work as a therapist, he enjoys working with individuals, couples and families. Some of the struggles that his clients come with are: anxiety, depression, anger, fear, loneness, lack of confidence, guilt, shame, minority issues. Jerry also works with men wanting help with issues like substance abuse, pornography, and dysfunctional familial dynamics. Often he finds that his patient’s lives have been effected by past and/or current traumas. Jerry’s special interest is working with pastors and church staff regarding personal and ministry issues.</p>
+            <p className={styles.body}>Jerry has been married 40 plus years with three married children and five grandchildren. After growing up on a farm, he and his wife began their life together in the farming business. Throughout all the career changes, their faith in God has always been what they have leaned on for strength.</p>
+          </div>
+
+        </div>
+      </div>
+      <div id="services" className={styles.services}>
 
       </div>
-    </div>
+      <div id="contact" className={styles.contact}>
+
+      </div>
+
+    </section>
   </div>
 )
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import Logo from '../../images/mpulse_logo.svg';
 import styles from './Navbar.css';
+
+// const scrollTo = (elID) =>  {
+//     const dest = document.getElementById(elID);
+//     const to = dest.offsetTop;
+//     const duration = 500;
+//     if (document.body.scrollTop === to) return;
+//     var diff = to - document.body.scrollTop;
+//     var scrollStep = Math.PI / (duration / 10);
+//     var count = 0, currPos;
+//     const start = dest.scrollTop;
+//     const scrollInterval = setInterval(function(){
+//         if (document.body.scrollTop !== to) {
+//             count = count + 1;
+//             currPos = start + diff * (0.5 - 0.5 * Math.cos(count * scrollStep));
+//             document.body.scrollTop = currPos;
+//         }
+//         else { clearInterval(scrollInterval); }
+//     },1000);
+// }
 
 const NavBar = () => (
 
   <nav className={[styles.navbar, styles.nav].join(' ')}>
     <ul className={styles.nav_ul}>
-      <li className={styles.link_left}><Link className={styles.link} to={"/"}><img className="img-responsive logo" src={Logo} alt="logo"></img></Link></li>
-      <li className={[styles.link_right].join(' ')} ><Link className={styles.link} to={"/ImagePage"}>ImagePage</Link></li>
-      <li className={[styles.link_right].join(' ')} ><Link className={styles.link} to={"/LinksPage"}>LinksPage</Link></li>
-      <li className={[styles.link_right].join(' ')} ><Link className={styles.link} to={"/MyApp"}>MyApp</Link></li>
+      <li className={[styles.link_right].join(' ')} ><a className={styles.link} href="#services">Services</a></li>
+      <li className={[styles.link_right].join(' ')} ><a className={styles.link} href="#contact">Contact</a></li>
     </ul>
   </nav>
 
