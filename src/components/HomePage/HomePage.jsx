b223898856ce06ab4e7ceac1baf86cc53fa436b4
Kyle Davelaar <kyledavelaar@gmail.com>
2021-08-29 15:30:45 -0700

    add about section

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import styles from './HomePage.css';
-import JerryThumb from '../../images/jerry-thumb2.jpeg';
+import JerryThumb from '../../images/jerry-davelaar.jpeg';
 
 const HomePage = () => (
   <div id={styles.wrapper}>
@@ -21,6 +21,18 @@ const HomePage = () => (
             <p className={styles.body}>Jerry enjoys working with individuals, couples and families. Some of the struggles that his clients come with are: anxiety, depression, anger, fear, loneness, lack of confidence, guilt, shame, minority issues. While working with men, Jerry helps with issues like substance abuse, pornography, and dysfunctional familial dynamics. Often he finds that his patient’s lives have been effected by past and/or current traumas. Jerry’s special interest is working with pastors and church staff regarding personal and ministry issues.</p>
             <p className={styles.body}>Jerry has been married 40 plus years with three married children and five grandchildren. After growing up on a farm, he and his wife began their life together in the farming business. Throughout all the career changes, their faith in God has always been what they have leaned on for strength.</p>
           </div>
+        </div>
+      </section>
+      <section id="about" className={styles.servicesContainer}>
+        <div className={styles.sectionHeaderContainer}>
+          <hr className={styles.headerLine}/>
+          <h1 className={styles.sectionHeader}>ABOUT</h1>
+          <hr className={styles.headerLine}/>
+        </div>
+        <div className={styles.aboutContainer}>
+          <p className={styles.body}>After working as a therapist for a few years, Jerry served for five years as an ordained associate pastor and then ten years as a senior pastor. Since then, Jerry has returned to his work as a therapist, pursuing his passion to understand people and bring hope to their desire for change and growth.</p>
+          <p className={styles.body}>Jerry enjoys working with individuals, couples and families. Some of the struggles that his clients come with are: anxiety, depression, anger, fear, loneness, lack of confidence, guilt, shame, minority issues. While working with men, Jerry helps with issues like substance abuse, pornography, and dysfunctional familial dynamics. Often he finds that his patient’s lives have been effected by past and/or current traumas. Jerry’s special interest is working with pastors and church staff regarding personal and ministry issues.</p>
+          <p className={styles.body}>Jerry has been married 40 plus years with three married children and five grandchildren. After growing up on a farm, he and his wife began their life together in the farming business. Throughout all the career changes, their faith in God has always been what they have leaned on for strength.</p>
         </div>
       </section>
       <section id="services" className={styles.servicesContainer}>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './Navbar.css';
-import Logo from '../../images/jd_logo.png';
+import Logo from '../../images/jerry_davelaar_logo.png';
 
 
 const NavBar = () => (
   <nav className={[styles.navbar, styles.nav].join(' ')}>
     <ul className={styles.nav_ul}>
       <li className={[styles.link_right, styles.logo].join(' ')}><img src={Logo} alt="Jerry Davelaar Christian Counseling" /></li>
+      <li className={[styles.link_right].join(' ')} ><a className={styles.link} href="#about">About</a></li>
       <li className={[styles.link_right].join(' ')} ><a className={styles.link} href="#services">Services</a></li>
       <li className={[styles.link_right].join(' ')} ><a className={styles.link} href="#contact">Contact</a></li>
     </ul>
