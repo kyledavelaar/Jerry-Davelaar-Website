e80b43be789b8d065f59005dcfe935d3a87e6752
Kyle Davelaar <kyledavelaar@gmail.com>
2021-08-29 15:11:10 -0700

    add favicon and adjust css

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './Navbar.css';
+import Logo from '../../images/jd_logo.png';
+
 
 const NavBar = () => (
-
   <nav className={[styles.navbar, styles.nav].join(' ')}>
     <ul className={styles.nav_ul}>
+      <li className={styles.link_right}><img src={Logo} alt="Jerry Davelaar Christian Counseling" /></li>
       <li className={[styles.link_right].join(' ')} ><a className={styles.link} href="#services">Services</a></li>
       <li className={[styles.link_right].join(' ')} ><a className={styles.link} href="#contact">Contact</a></li>
     </ul>
